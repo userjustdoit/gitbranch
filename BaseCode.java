476c49c796d160500530faefad0daa152f846640
tomlikecat <tomlikecat@tomlikecat.com>
2024-01-09 09:40:05 +0800

    edit mobile rebase

--- BaseCode.java
+++ BaseCode.java
@@ -3,7 +3,7 @@ public class BaseCode {
     public static void main(String[] args) {
         //code 1 edit in main
         //code update main
-
+        //code edit in mobile
         //base主干代码更新了
     }
 
